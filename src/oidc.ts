e8d2093ecbf5daca2d8c1c62909a61c4fbac4c4f
Bryan Abate <bryan.abate@gmail.com>
2024-09-17 13:40:21 +0200

    Make decodeIdToken generic

--- src/oidc.ts
+++ src/oidc.ts
@@ -1,9 +1,9 @@
 import { parseJWT } from "@oslojs/jwt";
 
-export function decodeIdToken(idToken: string): object {
+export function decodeIdToken<T extends object>(idToken: string): T {
 	try {
 		const [_header, payload, _signature] = parseJWT(idToken);
-		return payload;
+		return payload as T;
 	} catch (e) {
 		throw new Error("Invalid ID token", {
 			cause: e
